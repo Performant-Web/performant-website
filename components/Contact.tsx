61af693275789bb1e90a58625c79aaf5dc95b7db
Mike Melgren <mikemelgren@gmail.com>
2022-08-09 15:03:16 -0500

    testing netlify forms

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -41,25 +41,94 @@ export default function Contact() {
             <Box borderRadius="lg">
               <Box color="#0B0E3F">
                 <form
+                  style={{ width: '100%' }}
                   method="POST"
                   name="contact-form"
+                  data-netlify="true"
                   action="contact/?success=true"
-                  data-netlify="true">
+                >
                   <input type="hidden" name="form-name" value="contact-form" />
-                  <label htmlFor="name">Name *</label>
-                  <input
-                    id="name"
-                    name="name"
-                    required
-                    type="text"
-                  />
-                  <label htmlFor="company">Company *</label>
-                  <input id="company" name="company" required type="text" />
-                  <label htmlFor="email">E-mail Address *</label>
-                  <input id="email" type="email" name="email" required />
-                  <label htmlFor="message">Message *</label>
-                  <textarea id="message" name="message" required></textarea>
-                  <button type="submit">Submit</button>
+                  <FormControl id="contact-form-control" pb="8">
+                    <FormLabel id="contact-email"
+                      color={useColorModeValue('gray.600', 'gray.300')}
+                    >Email</FormLabel>
+                    <Input type="text"
+                      name='email'
+                      color={useColorModeValue('gray.600', 'gray.300')}
+                      borderColor={useColorModeValue('gray.300', 'rgba(135,135,135,0.5)')}
+                      size="md"
+                      rounded='none'
+                      mb='8'
+                      placeholder='Enter your email...'
+                      _focus={{
+                        borderWidth: '2px',
+                        borderColor: 'rgba(125,125,125,.75)',
+                      }} />
+                    <FormLabel id='contact-message'
+                      color={useColorModeValue('gray.600', 'gray.300')}
+                    >Message</FormLabel>
+                    <Textarea
+                      name='message'
+                      height='150'
+                      color={useColorModeValue('gray.600', 'gray.300')}
+                      borderColor={useColorModeValue('gray.300', 'rgba(135,135,135,0.5)')}
+                      rounded='none'
+                      _hover={{
+                        borderRadius: 'gray.300',
+                      }}
+                      _focus={{
+                        borderWidth: '2px',
+                        borderColor: 'rgba(125,125,125,.75)',
+                      }}
+                      placeholder="Enter your message..."
+                    />
+                    <HStack w='full' mt='8' display='flex' justify='space-between'>
+                      <Button
+                        name='submit'
+                        type='submit'
+                        variant="solid"
+                        bg="red.600"
+                        px={8}
+                        py={6}
+                        letterSpacing='1px'
+                        fontWeight='600'
+                        fontSize='lg'
+                        _hover={{
+                          bg: 'red.500'
+                        }}
+                        color="white"
+                        rounded='none'>
+                        Send
+                      </Button>
+                      <HStack
+                        mt={{ lg: 10, md: 10 }}
+                        spacing={5}
+                        alignItems="center">
+                        <Link passHref href='https://github.com/Performant-Web'>
+                          <IconButton
+                            color={useColorModeValue('gray.700', 'gray.300')}
+                            aria-label="github"
+                            variant="ghost"
+                            size="lg"
+                            rounded='none'
+                            _hover={{ color: useColorModeValue('gray.900', 'gray.100') }}
+                            icon={<BsGithub size="28px" />}
+                          />
+                        </Link>
+                        <Link passHref href='https://discord.com/users/895421310828695563'>
+                          <IconButton
+                            color={useColorModeValue('gray.700', 'gray.300')}
+                            aria-label="discord"
+                            variant="ghost"
+                            size="lg"
+                            rounded='none'
+                            _hover={{ color: useColorModeValue('gray.900', 'gray.100') }}
+                            icon={<BsDiscord size="28px" />}
+                          />
+                        </Link>
+                      </HStack>
+                    </HStack>
+                  </FormControl>
                 </form>
               </Box>
             </Box>
