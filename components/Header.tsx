369e7ae4d7d25f5c3a398339468700ce454e07de
Mike Melgren <mikemelgren@gmail.com>
2022-08-09 09:29:58 -0500

    logo responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,72 +37,70 @@ export default function Header() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <>
-      <Box px={4}>
-        <Flex w='85%' mx='auto' h={48} alignItems='center' justifyContent='space-between'>
-          <Link display='flex' flexDirection='row' alignItems='center' _hover={{ textDecoration: 'none', transform: 'scale(1.03, 1.03)', color: useColorModeValue('gray.700', 'gray.100') }}>
-            <Image src={colorMode === 'dark' ? './logo.png' : './logo-dark.png'} h='34px' mb='-1px' w='auto' mr='-1px' opacity='0.93' />
-            <Box letterSpacing='-1.0px' fontSize='48' fontWeight='700'>erformant</Box>
-          </Link>
-          <Flex alignItems={'center'}>
-            <IconButton
-              size={'lg'}
-              rounded='none'
-              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-              aria-label={'Open Menu'}
-              display={{ lg: 'none' }}
-              onClick={isOpen ? onClose : onOpen}
-            />
-            <Stack direction={'row'} spacing={7}>
-              <HStack
-                as={'nav'}
-                spacing={8}
-                display={{ base: 'none', lg: 'flex' }}>
-                {Links.map((link) => (
-                  <NavLink key={link}>{link}</NavLink>
-                ))}
-                <Button
-                  p={6}
-                  onClick={toggleColorMode}
-                  color={useColorModeValue('gray.600', 'gray.300')}
-                  rounded='none'
-                  bg='none'
-                  _hover={{
-                    bg: 'none',
-                    color: useColorModeValue('gray.900', 'gray.100')
-                  }}>
-                  {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-                </Button>
-              </HStack>
-            </Stack>
-          </Flex>
-        </Flex>
-
-        {isOpen ? (
-          <Box px={4} mr={4} pb={4} display={{ lg: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
+    <Box px={4}>
+      <Flex w='85%' mx='auto' h={48} alignItems='center' justifyContent='space-between'>
+        <Link display='flex' flexDirection='row' alignItems='center' _hover={{ textDecoration: 'none', transform: 'scale(1.03, 1.03)', color: useColorModeValue('gray.700', 'gray.100') }}>
+          <Image src={colorMode === 'dark' ? './logo.png' : './logo-dark.png'} h={{ base: '26px', md: '34px' }} mb={{ base: '2px', md: '-1px' }} w='auto' mr='-1px' opacity='0.93' />
+          <Box letterSpacing='-1.0px' fontSize={{ base: '36', md: '48' }} fontWeight='700'>erformant</Box>
+        </Link>
+        <Flex alignItems={'center'}>
+          <IconButton
+            size={'lg'}
+            rounded='none'
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ lg: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <Stack direction={'row'} spacing={7}>
+            <HStack
+              as={'nav'}
+              spacing={8}
+              display={{ base: 'none', lg: 'flex' }}>
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
-              <Box px={4}>
-                <Button
-                  p={6}
-                  w={12}
-                  onClick={toggleColorMode}
-                  color={useColorModeValue('gray.600', 'gray.300')}
-                  rounded='none'
-                  bg='none'
-                  _hover={{
-                    bg: 'none',
-                    color: useColorModeValue('gray.900', 'gray.100')
-                  }}>
-                  {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-                </Button>
-              </Box>
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </>
+              <Button
+                p={6}
+                onClick={toggleColorMode}
+                color={useColorModeValue('gray.600', 'gray.300')}
+                rounded='none'
+                bg='none'
+                _hover={{
+                  bg: 'none',
+                  color: useColorModeValue('gray.900', 'gray.100')
+                }}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </HStack>
+          </Stack>
+        </Flex>
+      </Flex>
+
+      {isOpen ? (
+        <Box px={4} mr={4} pb={4} display={{ lg: 'none' }}>
+          <Stack as={'nav'} spacing={4}>
+            {Links.map((link) => (
+              <NavLink key={link}>{link}</NavLink>
+            ))}
+            <Box px={4}>
+              <Button
+                p={6}
+                w={12}
+                onClick={toggleColorMode}
+                color={useColorModeValue('gray.600', 'gray.300')}
+                rounded='none'
+                bg='none'
+                _hover={{
+                  bg: 'none',
+                  color: useColorModeValue('gray.900', 'gray.100')
+                }}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Box>
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
   );
 }
