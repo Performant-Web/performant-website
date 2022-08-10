c78e28a60ee75a8c366acb888627e431b6de5dc4
Mike Melgren <mikemelgren@gmail.com>
2022-08-10 14:26:35 -0500

    google fonts and page head title and meta

--- pages/_document.js
+++ pages/_document.js
@@ -4,24 +4,44 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-      <script
+        <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=G-R2F19FE31M`}
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-R2F19FE31M', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-R2F19FE31M', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap"
           />
-      <title>Performant Web Development</title>
-        <meta name="description" content="Performant Web Development Services. Solving your business problems with technology." />
+        </noscript>
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import Landing from '../components/Landing';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Performant Web Development</title>
+        <meta name="description" content="Performant Web Development Services. Solving your business problems with technology." />
+      </Head>
       <Landing />
       <About />
       <Projects />
