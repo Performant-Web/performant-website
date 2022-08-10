87dd170f5f0b2fbf876d9883b357f7d5c503f761
Mike Melgren <mikemelgren@gmail.com>
2022-08-10 10:40:58 -0500

    added title and html lang

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import Landing from '../components/Landing';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Performant Web Development</title>
+        <meta></meta>
+      </Head>
       <Landing />
       <About />
       <Projects />
