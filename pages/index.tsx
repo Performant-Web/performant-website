08bc1833d145718ad3ad5fd8723bad2ce1dc0b5a
Mike Melgren <mikemelgren@gmail.com>
2022-08-10 11:01:46 -0500

    removed next head from page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,10 @@ import Landing from '../components/Landing';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
-import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home</title>
-        <meta name="description" content="Homepage for Performant Web Development Services." />
-      </Head>
       <Landing />
       <About />
       <Projects />
