e36adbcfb05056a392f1769a7116c53e5506a376
Mike Melgren <mikemelgren@gmail.com>
2022-08-10 13:38:07 -0500

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,22 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+      <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-R2F19FE31M`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R2F19FE31M', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       <title>Performant Web Development</title>
         <meta name="description" content="Performant Web Development Services. Solving your business problems with technology." />
       </Head>
