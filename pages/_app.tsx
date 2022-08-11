587f11f31f571560cd16af6d0a6c58a2f8df7777
Mike Melgren <mikemelgren@gmail.com>
2022-08-11 09:07:53 -0500

    removed next head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import type { AppProps } from 'next/app'
 import '@fontsource/source-sans-pro/400.css'
 import '@fontsource/source-sans-pro/600.css'
 import '@fontsource/source-sans-pro/700.css'
-import Head from 'next/head'
 import Script from 'next/script'
 
 //focus outline - a11y
@@ -46,15 +45,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','G-R2F19FE31M');`}}>
-        </Script>
-      </Head>
+      </Script>
       <ChakraProvider theme={theme}>
         <Header />
         <Component {...pageProps} />
