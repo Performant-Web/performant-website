9f8b55cace205f9f040287758148a16c4e5e4965
Mike Melgren <mikemelgren@gmail.com>
2022-08-11 08:58:07 -0500

    moved google script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import type { AppProps } from 'next/app'
 import '@fontsource/source-sans-pro/400.css'
 import '@fontsource/source-sans-pro/600.css'
 import '@fontsource/source-sans-pro/700.css'
+import Head from 'next/head'
+import Script from 'next/script'
 
 //focus outline - a11y
 const focusShadow = '0 0 0 2px rgba(125,125,125,.75)'
@@ -43,10 +45,21 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-    <ChakraProvider theme={theme}>
-      <Header />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','G-R2F19FE31M');`}}>
+        </Script>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','G-R2F19FE31M');`}}></Script>
       </Head>
       <body>
         <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-R2F19FE31M"
