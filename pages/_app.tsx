55944895f68431762a72306eb0bf7b2bcf40c4e1
Mike Melgren <mikemelgren@gmail.com>
2022-08-11 09:11:37 -0500

    moved gtag to custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import type { AppProps } from 'next/app'
 import '@fontsource/source-sans-pro/400.css'
 import '@fontsource/source-sans-pro/600.css'
 import '@fontsource/source-sans-pro/700.css'
-import Script from 'next/script'
 
 //focus outline - a11y
 const focusShadow = '0 0 0 2px rgba(125,125,125,.75)'
@@ -45,13 +44,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script id='gtag' strategy="afterInteractive" dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','G-R2F19FE31M');`}}>
-      </Script>
       <ChakraProvider theme={theme}>
         <Header />
         <Component {...pageProps} />
