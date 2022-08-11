e5a2c4a38c2ec6778aeedc3132bf5ad0e1b9f4b1
Mike Melgren <mikemelgren@gmail.com>
2022-08-11 09:09:04 -0500

    added id to script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,7 +45,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+      <Script id='gtag' strategy="afterInteractive" dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
