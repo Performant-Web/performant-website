0de96b33ecfe3448ea0068ea673837e460540902
Mike Melgren <mikemelgren@gmail.com>
2022-08-11 08:48:47 -0500

    defer google tag scripts until after interactive

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-R2F19FE31M`}
-          />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-R2F19FE31M', {
-            page_path: window.location.pathname,
-          });
-        `,
-          }}
-        />
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','G-R2F19FE31M');`}}></Script>
       </Head>
       <body>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-R2F19FE31M"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}>
+        </noscript>
         <Main />
         <NextScript />
       </body>
