b68ad4c909ab17f7c32e899160198f16a435bc34
Mike Melgren <mikemelgren@gmail.com>
2022-09-27 15:34:05 -0500

    removed blog from nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
 
   return (
     <Box position='absolute' left='0' right='0' top={0} maxW='1456px' mx='auto' bg={bg} zIndex='5'>
-      <Flex pr='8' position='relative' w='100%' justify='space-between' h={{ base: '24', sm: '48' }} alignItems='center' justifyContent='space-between'>
+      <Flex pr={{ base: '0', xl: '4' }} position='relative' w='100%' justify='space-between' h={{ base: '24', sm: '48' }} alignItems='center' justifyContent='space-between'>
         <Link pl={{ base: '5', sm: '8' }} ml={{ base: '0px', lg: '-10px' }} href='/' passHref display='flex' flexDirection='row' alignItems='center' _hover={{ textDecoration: 'none', color: useColorModeValue('gray.700', 'gray.100') }}>
           <Box pr='8' letterSpacing='-1.0px' fontSize={{ base: '36', sm: '48' }} fontWeight='700'>Performant</Box>
         </Link>
@@ -86,7 +86,7 @@ export default function Header() {
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
-              <NextLink href={'https://blog.performantweb.dev'} passHref>
+              {/*<NextLink href={'https://blog.performantweb.dev'} passHref>
                 <Link
                   textTransform='capitalize'
                   h='full'
@@ -102,7 +102,7 @@ export default function Header() {
                   }}>
                   Blog
                 </Link >
-              </NextLink>
+                </NextLink>*/}
             </HStack>
           </Stack>
         </Flex>
